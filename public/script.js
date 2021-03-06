de176b7b29dfa9253e7e3b7144ac3f961843932c
Mia Yamada <ymd99mh@umd.edu>
2021-03-05 20:25:03 -0500

    js v2

--- public/script.js
+++ public/script.js
@@ -1,37 +1,74 @@
-const endpoint = '';
+async function windowActions() {
+    console.log('window loaded');
+    const form = document.querySelector('.userform');
+    const search = document.querySelector('#name');
+    const request = await fetch('/api');
 
-const cities = [];
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log('submit fired', search.value);
+        const display = data.filter((record) => record.city.toUpperCase() === search.value.toUpperCase)
 
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(blob => cities.push(...data)) 
+        // ^display is the filtered dataset
 
-function findMatches(wordToMatch, cites){
-    return cities.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex)
+        // HOW TO WRITE POST REQUEST
+
+        // const request = await fetch('/api', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({data: search.value})
+        // });
+        // const data = await request.json();
+
     });
-    // ^filter down into subset
+
+    search.addEventListener('input', (event) => {
+        console.log('input', event.target.value);
+    });
 }
 
-function displayMatches(){
-    const matchArray = findMatches(this.value, cities);
-    const html = matchArray.map(place => {
-        const regex = new RegExp(this.value, 'gi');
-        const cityName = place.city.replace(regex, `<span clas = 'h1> $ {this.value}</spen>`);
-        const stateName = place.state.replace(regex, `<span clas = 'h1> $ {this.value}</spen>`)
-        return `
-        <li>
-            <span class = 'name'> $ {cityName}, 4{stateName}</span>
-            <span class = 'population'> $ {numberWithCommas(place.population)}</span>  
-        </li>
-        `;
-    }).join('');
-    suggestions.innerHTML = html;
-}
+winndow.onload = windowActions;
 
-const searchInput = document.querySelector('.search');
-const searchInput = document.querySelector('.suggestions');
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+
+
+
+// const endpoint = '';
+
+// const cities = [];
+
+// fetch(endpoint)
+//     .then(blob => blob.json())
+//     .then(blob => cities.push(...data)) 
+
+// function findMatches(wordToMatch, cites){
+//     return cities.filter(place => {
+//         const regex = new RegExp(wordToMatch, 'gi');
+//         return place.city.match(regex) || place.state.match(regex)
+//     });
+//     // ^filter down into subset
+// }
+
+// function displayMatches(){
+//     const matchArray = findMatches(this.value, cities);
+//     const html = matchArray.map(place => {
+//         const regex = new RegExp(this.value, 'gi');
+//         const cityName = place.city.replace(regex, `<span clas = 'h1> $ {this.value}</spen>`);
+//         const stateName = place.state.replace(regex, `<span clas = 'h1> $ {this.value}</spen>`)
+//         return `
+//         <li>
+//             <span class = 'name'> $ {cityName}, 4{stateName}</span>
+//             <span class = 'population'> $ {numberWithCommas(place.population)}</span>  
+//         </li>
+//         `;
+//     }).join('');
+//     suggestions.innerHTML = html;
+// }
+
+// const searchInput = document.querySelector('.search');
+// const searchInput = document.querySelector('.suggestions');
+
+// searchInput.addEventListener('change', displayMatches);
+// searchInput.addEventListener('keyup', displayMatches);
